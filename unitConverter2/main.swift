4853fb2979a80a2449bc87839ac50c9b518b61d4
Dongmin.yoon <ehdals8965@naver.com>
2018-09-14 13:46:41 +0900

    단위 변환기2

--- unitConverter2/main.swift
+++ unitConverter2/main.swift
@@ -13,32 +13,32 @@ let convertcmToinchValue : Float = 0.393
 let convertcmToYard : Float = 0.0109
 
 
-func centimeterToMeter(_ originNumber : Float) -> Float{
+func centimeterToMeter(_ originNumber : Float) -> Float{                                                    // cm -> m
     return originNumber / convertValuecmTom
 }
 
-func meterTocentimeter(_ originNumber : Float) -> Float{
+func meterTocentimeter(_ originNumber : Float) -> Float{                                                    // m -> cm
     return originNumber * convertValuecmTom
 }
 
-func centimeterToinch(_ originNumber : Float) -> Float{
+func centimeterToinch(_ originNumber : Float) -> Float{                                                     // cm -> inch
     return originNumber * convertcmToinchValue
 }
 
-func inchTocentimeter(_ originNumber : Float) -> Float{
+func inchTocentimeter(_ originNumber : Float) -> Float{                                                     // inch -> cm
     return originNumber / convertcmToinchValue
 }
 
-func centimeterToyard(_ originNumber : Float) -> Float{
+func centimeterToyard(_ originNumber : Float) -> Float{                                                     // cm -> yard
     return originNumber * convertcmToYard
 }
 
-func yardTocentimeter(_ originNumber : Float) -> Float{
+func yardTocentimeter(_ originNumber : Float) -> Float{                                                     // yard -> cm
     return originNumber / convertcmToYard
 }
 
-func enterUserInput() -> (number : String, UnitOne : String, UnitTwo : String){
-    
+func enterUserInput() -> (number : String, UnitOne : String, UnitTwo : String){                             // 사용자 입력 받는부분
+
     let enterNumberUnit : String = readLine()!
     var numberUnitSet = enterNumberUnit.components(separatedBy: " ")
     
@@ -54,47 +54,54 @@ func enterUserInput() -> (number : String, UnitOne : String, UnitTwo : String){
     else{
         return ("", "", "")
     }
+    
 }
 
-while true{
-    let numberUnitSet = enterUserInput()
+func unitConvert(_ originnumber : String, _ UnitOne : String, _ UnitTwo : String){                          // 단위 변환해주는 함수
+    
     var convertNumber : Float
     
-    if(numberUnitSet.number == "q" || numberUnitSet.number == "quit"){
-        break;
-    }
-
-    switch (numberUnitSet.UnitOne, numberUnitSet.UnitTwo){
+    switch (UnitOne, UnitTwo){
     case("yard", "m"):
-        convertNumber = centimeterToMeter(yardTocentimeter(Float(numberUnitSet.number)!))
+        convertNumber = centimeterToMeter(yardTocentimeter(Float(originnumber)!))
         print("\(convertNumber)m")
     case("yard", _):
-        convertNumber = centimeterToMeter(yardTocentimeter(Float(numberUnitSet.number)!))
+        convertNumber = centimeterToMeter(yardTocentimeter(Float(originnumber)!))
         print("\(convertNumber)m")
     case("m", "yard"):
-        convertNumber = centimeterToyard(meterTocentimeter(Float(numberUnitSet.number)!))
+        convertNumber = centimeterToyard(meterTocentimeter(Float(originnumber)!))
         print("\(convertNumber)yard")
     case("m", "inch"):
-        convertNumber = centimeterToinch(meterTocentimeter(Float(numberUnitSet.number)!))
+        convertNumber = centimeterToinch(meterTocentimeter(Float(originnumber)!))
         print("\(convertNumber)inch")
     case("inch", "m"):
-        convertNumber = centimeterToMeter(inchTocentimeter(Float(numberUnitSet.number)!))
+        convertNumber = centimeterToMeter(inchTocentimeter(Float(originnumber)!))
         print("\(convertNumber)m")
     case("inch", "cm"):
-        convertNumber = inchTocentimeter(Float(numberUnitSet.number)!)
+        convertNumber = inchTocentimeter(Float(originnumber)!)
         print("\(convertNumber)cm")
     case("cm", "inch"):
-        convertNumber = centimeterToinch(Float(numberUnitSet.number)!)
+        convertNumber = centimeterToinch(Float(originnumber)!)
         print("\(convertNumber)inch")
     case("cm", _):
-        convertNumber = centimeterToMeter(Float(numberUnitSet.number)!)
+        convertNumber = centimeterToMeter(Float(originnumber)!)
         print("\(convertNumber)m")
     case("m", _):
-        convertNumber = meterTocentimeter(Float(numberUnitSet.number)!)
+        convertNumber = meterTocentimeter(Float(originnumber)!)
         print("\(convertNumber)cm")
     case(_, _):
         print("지원하지 않는 단위 입니다.")
     }
+}
+
+while true{
+    let numberUnitSet = enterUserInput()
+    
+    if(numberUnitSet.number == "q" || numberUnitSet.number == "quit"){
+        break
+    }
+    
+    unitConvert(numberUnitSet.number, numberUnitSet.UnitOne, numberUnitSet.UnitTwo)
 }
 
 
